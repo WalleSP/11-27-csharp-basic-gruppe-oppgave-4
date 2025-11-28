04c1c096680f62111e5b6698c7764a4ef150fcd8
hro1025 <hro102025@gmail.com>
2025-11-28 09:33:40 +0100

    TouchCommand fix

--- Classes/Controllers/TouchCommand.cs
+++ Classes/Controllers/TouchCommand.cs
@@ -1,22 +1,22 @@
+using System.Collections;
+
 namespace _11_27_csharp_basic_gruppe_oppgave_4
 {
-
-
-/// <summary>
-/// Touch command from UNIX that creates a new file.
-/// </summary>
-public class TouchCommand
-{
-    public void Touch(string filePath)
+    /// <summary>
+    /// Touch command from UNIX that creates a new file.
+    /// </summary>
+    public class TouchCommand
     {
-        if (File.Exists(filePath))
+        public void Touch(string filePath)
         {
-            Console.WriteLine("File already Exists");
-        }
-        else
-        {
-            File.Create(filePath);
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File already Exists");
+            }
+            else
+            {
+                File.Create(filePath).Close();
+            }
         }
     }
-}
 }
