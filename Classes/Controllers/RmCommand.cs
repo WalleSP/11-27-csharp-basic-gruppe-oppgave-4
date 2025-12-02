61873f13b7cf9b0ec25e20eafe3a8c16cd72838b
hro1025 <hro102025@gmail.com>
2025-12-02 10:19:47 +0100

    added '-r' argument for rm command and fix ls

--- Classes/Controllers/RmCommand.cs
+++ Classes/Controllers/RmCommand.cs
@@ -2,16 +2,36 @@ namespace _11_27_csharp_basic_gruppe_oppgave_4;
 
 public static class RmCommand
 {
-    public static void Rm(string fileName)
+    public static void Rm(string newArgs, string name)
     {
-        var file = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-        if (File.Exists(file))
+        var directoryName = Path.GetDirectoryName(newArgs);
+        var file = Path.Combine(Directory.GetCurrentDirectory(), name);
+
+        if (newArgs.Contains("-r"))
         {
-            File.Delete(file);
+            {
+                Console.WriteLine("Invalid path");
+            }
+
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+            }
+            else
+            {
+                Console.WriteLine($"The directory {directoryName} allready exists");
+            }
         }
         else
         {
-            Console.WriteLine($"There is no file with the name {fileName} ");
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+            else
+            {
+                Console.WriteLine($"There is no file with the name {name}");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -95,7 +95,7 @@ namespace _11_27_csharp_basic_gruppe_oppgave_4
                     break;
 
                 case "ls":
-                    lsCommand.ls(newArgs);
+                    lsCommand.ls(newArgs[1]);
                     break;
 
                 case "cp":
@@ -116,7 +116,7 @@ namespace _11_27_csharp_basic_gruppe_oppgave_4
                 case "rm":
                     if (newArgs.Length > 1)
                     {
-                        RmCommand.Rm(newArgs[1]);
+                        RmCommand.Rm(newArgs[1], newArgs[2]);
                     }
                     else
                     {
