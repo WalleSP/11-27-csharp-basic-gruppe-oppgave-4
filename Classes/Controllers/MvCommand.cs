ddb1016e33c544dcfe9f5e0e4f37a0743aeec940
hro1025 <hro102025@gmail.com>
2025-12-03 14:22:21 +0100

    added move function

--- Classes/Controllers/MvCommand.cs
+++ Classes/Controllers/MvCommand.cs
@@ -1,8 +1,29 @@
+namespace _11_27_csharp_basic_gruppe_oppgave_4;
+
 public class MvCommand
 {
-    public static void Mv(string fileName, string filePath)
+    public static void Mv(string source, string dest)
     {
-        File.Move(fileName, filePath);
-        Console.WriteLine($"Your file '{fileName}' got moved to '{filePath}'");
+        if (Directory.Exists(dest))
+        {
+            string fileName = Path.GetFileName(source);
+            dest = Path.Combine(dest, fileName);
+        }
+
+        if (Directory.Exists(source))
+        {
+            Directory.Move(source, dest);
+            Console.WriteLine($"Directory moved to '{dest}'");
+        }
+
+        if (File.Exists(source))
+        {
+            File.Move(source, dest, true);
+            Console.WriteLine($"Your file '{source}' moved successfully");
+        }
+        else
+        {
+            Console.WriteLine($"Your '{source}' not found");
+        }
     }
 }
