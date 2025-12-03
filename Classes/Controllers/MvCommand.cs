74f1f666bc14561e8677024a672509eaf14af4af
hro1025 <hro102025@gmail.com>
2025-12-03 13:33:25 +0100

    added an line in Mv command

--- Classes/Controllers/MvCommand.cs
+++ Classes/Controllers/MvCommand.cs
@@ -3,5 +3,6 @@ public class MvCommand
     public static void Mv(string fileName, string filePath)
     {
         File.Move(fileName, filePath);
+        Console.WriteLine($"Your file '{fileName}' got moved to '{filePath}'");
     }
 }
